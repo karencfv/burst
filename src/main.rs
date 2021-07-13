0758bce72dc0620879ede4bd0b349f8b22f35215
karencfv <karencfv@gmail.com>
2021-07-13 18:48:17 +1200

    Use Rc to stop cloning Client every time a task is spawned

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 extern crate clap;
 
+use std::rc::Rc;
+
 use usdt::dtrace_provider;
 
 use app::burst_app;
@@ -13,6 +15,8 @@ dtrace_provider!("src/burst.d");
 
 #[tokio::main]
 async fn main() {
-    let client = burst_app();
+    // Using Rc instead of Arc as none of the values are being mutated,
+    // it's not necessary to pay the performance penalty.
+    let client = Rc::new(burst_app());
     client.send_load().await;
 }
