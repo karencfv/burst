9f5d27247abcf3dafe800921f36e102d29a50562
karencfv <karencfv@gmail.com>
2021-07-20 19:08:23 +1200

    Separate cmd() from burst_app()

--- src/app.rs
+++ src/app.rs
@@ -10,97 +10,97 @@ macro_rules! validate_flag_error {
     };
 }
 
-pub fn burst_app() -> Client {
+const LOAD_FLAG: &str = "load";
+const WORKERS_FLAG: &str = "workers";
+const DURATION_FLAG: &str = "duration";
+const INTERVAL_FLAG: &str = "interval";
+const TIMEOUT_FLAG: &str = "timeout";
+const HOST_FLAG: &str = "host";
+const USER_FLAG: &str = "user";
+const PASS_FLAG: &str = "pass";
+const EXACT_FLAG: &str = "exact";
+const VERBOSE_FLAG: &str = "verbose";
+
+fn cmd<'a, 'b>() -> App<'a, 'b> {
     let app = App::new("burst")
         .version("0.1-dev")
         .about("Sends bursts of requests to a specified host.");
 
-    let load_flag = "load";
-    let load_arg = Arg::with_name(load_flag)
-        .long(load_flag)
+    let load_arg = Arg::with_name(LOAD_FLAG)
+        .long(LOAD_FLAG)
         .short("l")
         .takes_value(true)
         .default_value("100")
         .help("Amount of requests to send.")
         .required(false);
 
-    let workers_flag = "workers";
-    let workers_arg = Arg::with_name(workers_flag)
-        .long(workers_flag)
+    let workers_arg = Arg::with_name(WORKERS_FLAG)
+        .long(WORKERS_FLAG)
         .short("w")
         .takes_value(true)
         .default_value("10")
         .help("Number of workers to run in parallel.")
         .required(false);
 
-    let duration_flag = "duration";
-    let duration_arg = Arg::with_name(duration_flag)
-        .long(duration_flag)
+    let duration_arg = Arg::with_name(DURATION_FLAG)
+        .long(DURATION_FLAG)
         .short("d")
         .takes_value(true)
         .help("Sends load for the given amount of time set in seconds.
 The actual running time will vary depending on the load, workers and the time it takes for the response to return.")
         .required(false);
 
-    let interval_flag = "interval";
-    let interval_arg = Arg::with_name(interval_flag)
-        .long(interval_flag)
+    let interval_arg = Arg::with_name(INTERVAL_FLAG)
+        .long(INTERVAL_FLAG)
         .short("i")
         .takes_value(true)
-        .requires(duration_flag)
+        .requires(DURATION_FLAG)
         .help("Interval time between bursts of requests in seconds. Requires --duration to be set.")
         .required(false);
 
-    let timeout_flag = "timeout";
-    let timeout_arg = Arg::with_name(timeout_flag)
-        .long(timeout_flag)
+    let timeout_arg = Arg::with_name(TIMEOUT_FLAG)
+        .long(TIMEOUT_FLAG)
         .short("t")
         .takes_value(true)
         .default_value("20")
         .help("Timeout in seconds for each request.")
         .required(false);
 
-    let host_flag = "host";
-    let host_arg = Arg::with_name(host_flag)
-        .long(host_flag)
+    let host_arg = Arg::with_name(HOST_FLAG)
+        .long(HOST_FLAG)
         .short("h")
         .takes_value(true)
         .help("Host header to send the requests to.")
         .required(true);
 
-    let user_flag = "user";
-    let user_arg = Arg::with_name(user_flag)
-        .long(user_flag)
+    let user_arg = Arg::with_name(USER_FLAG)
+        .long(USER_FLAG)
         .short("u")
         .takes_value(true)
         .help("User for basic authentication.")
         .required(false);
 
-    let pass_flag = "pass";
-    let pass_arg = Arg::with_name(pass_flag)
-        .long(pass_flag)
+    let pass_arg = Arg::with_name(PASS_FLAG)
+        .long(PASS_FLAG)
         .short("p")
         .takes_value(true)
         .help("Password for basic authentication.")
         .required(false);
 
-    let exact_flag = "exact";
-    let exact_arg = Arg::with_name(exact_flag)
-        .long(exact_flag)
+    let exact_arg = Arg::with_name(EXACT_FLAG)
+        .long(EXACT_FLAG)
         .short("e")
-        .requires(duration_flag)
+        .requires(DURATION_FLAG)
         .help("Starts a timer when using --duration. This means that the running time will be exact to the set duration time, but some requests may have not completed.")
         .required(false);
 
-    let verbose_flag = "verbose";
-    let verbose_arg = Arg::with_name(verbose_flag)
-        .long(verbose_flag)
+    let verbose_arg = Arg::with_name(VERBOSE_FLAG)
+        .long(VERBOSE_FLAG)
         .short("v")
         .help("Enable verbose mode.")
         .required(false);
 
-    let app = app
-        .arg(load_arg)
+    app.arg(load_arg)
         .arg(duration_arg)
         .arg(interval_arg)
         .arg(exact_arg)
@@ -109,25 +109,29 @@ The actual running time will vary depending on the load, workers and the time it
         .arg(workers_arg)
         .arg(pass_arg)
         .arg(verbose_arg)
-        .arg(user_arg);
+        .arg(user_arg)
+}
+
+pub fn burst_app() -> Client {
+    let app = cmd();
 
     let matches = app.get_matches();
 
     let load = matches
-        .value_of(load_flag)
-        .expect(validate_flag_error!(load_flag));
+        .value_of(LOAD_FLAG)
+        .expect(validate_flag_error!(LOAD_FLAG));
 
     let workers = matches
-        .value_of(workers_flag)
-        .expect(validate_flag_error!(workers_flag));
+        .value_of(WORKERS_FLAG)
+        .expect(validate_flag_error!(WORKERS_FLAG));
 
     let timeout = matches
-        .value_of(timeout_flag)
-        .expect(validate_flag_error!(timeout_flag));
+        .value_of(TIMEOUT_FLAG)
+        .expect(validate_flag_error!(TIMEOUT_FLAG));
 
     let host = matches
-        .value_of(host_flag)
-        .expect(validate_flag_error!(host_flag));
+        .value_of(HOST_FLAG)
+        .expect(validate_flag_error!(HOST_FLAG));
 
     let workers: usize = workers.parse().unwrap();
     let timeout: u64 = timeout.parse().unwrap();
@@ -138,53 +142,53 @@ The actual running time will vary depending on the load, workers and the time it
     // Sets duration and interval to 0 unless the duration flag has been used.
     // In which case load will be sent for a specified duration as opposed
     // to a single burst of n amount of requests.
-    let duration: u64 = if matches.is_present(duration_flag) {
+    let duration: u64 = if matches.is_present(DURATION_FLAG) {
         let time = matches
-            .value_of(duration_flag)
-            .expect(validate_flag_error!(duration_flag));
+            .value_of(DURATION_FLAG)
+            .expect(validate_flag_error!(DURATION_FLAG));
         let time: u64 = time.parse().unwrap();
         time
     } else {
         0
     };
 
-    let interval: u64 = if matches.is_present(interval_flag) {
+    let interval: u64 = if matches.is_present(INTERVAL_FLAG) {
         let interval_time = matches
-            .value_of(interval_flag)
-            .expect(validate_flag_error!(interval_flag));
+            .value_of(INTERVAL_FLAG)
+            .expect(validate_flag_error!(INTERVAL_FLAG));
         let interval_time: u64 = interval_time.parse().unwrap();
         interval_time
     } else {
         0
     };
 
-    let exact = if matches.is_present(exact_flag) {
+    let exact = if matches.is_present(EXACT_FLAG) {
         true
     } else {
         false
     };
 
-    let user = if matches.is_present(user_flag) {
+    let user = if matches.is_present(USER_FLAG) {
         let user_str = matches
-            .value_of(user_flag)
-            .expect(validate_flag_error!(user_flag));
+            .value_of(USER_FLAG)
+            .expect(validate_flag_error!(USER_FLAG));
         let user_str: String = user_str.parse().unwrap();
         user_str
     } else {
         String::from("")
     };
 
-    let pass = if matches.is_present(pass_flag) {
+    let pass = if matches.is_present(PASS_FLAG) {
         let pass_str = matches
-            .value_of(pass_flag)
-            .expect(validate_flag_error!(pass_flag));
+            .value_of(PASS_FLAG)
+            .expect(validate_flag_error!(PASS_FLAG));
         let pass_str: String = pass_str.parse().unwrap();
         Some(pass_str)
     } else {
         None
     };
 
-    let verbose = if matches.is_present(verbose_flag) {
+    let verbose = if matches.is_present(VERBOSE_FLAG) {
         true
     } else {
         false
